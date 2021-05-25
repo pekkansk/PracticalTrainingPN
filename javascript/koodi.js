128e06db9c1cd6ca95780de44a7fc00d9fd05859
pekkansk <pekkansk@gmail.com>
2021-05-25 13:46:33 +0300

    Taulukko ilman yksittäsiä sisältöjä

--- javascript/koodi.js
+++ javascript/koodi.js
@@ -10,6 +10,9 @@ var Leipahiirella = "Muutoksen tuulet puhaltaa"
 var Leipailmanhiirta = "Hello world!"
 var Leipa = document.getElementById("leipa")
 var Otsikko = document.getElementById("otsikko")
+var taulukonleveys = 3
+var taulukonkorkeus = 2
+var taulukonkoko = taulukonleveys * taulukonkorkeus
 
 
 function kuvat() {
@@ -18,7 +21,24 @@ function kuvat() {
   	IC.src = "https://i.ytimg.com/vi/hU_dcC3pvcU/maxresdefault.jpg";
 	var src = document.getElementById("ic");
 	src.appendChild(IC);
+
+	var poyta = document.createElement('table'),
+    	tr = document.createElement('tr'),
+    	cells, i;
+ 	for (i = 0; i < taulukonleveys; i++) { 
+    	tr.appendChild(document.createElement('td'));
 	}
+	for (i = 0; i < taulukonkorkeus; i++) { 
+    	poyta.appendChild(tr.cloneNode(true));
+	}
+	cells = poyta.getElementsByTagName('td'); // get all of the cells
+	for (i = 0; i < taulukonkoko; i++) {               // number them
+    	cells[1].innerHTML = "<img src=\'https://i.ytimg.com/vi/JC6KKLz8qHE/hqdefault.jpg'>";
+		cells[2].innerHTML = "<img src=\'https://i.ytimg.com/vi/hU_dcC3pvcU/maxresdefault.jpg'>";
+	}
+	document.getElementById('container').appendChild(poyta);
+}
+	
 	
 function ladattuna() {
 
