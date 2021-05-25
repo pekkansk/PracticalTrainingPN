7e94dcc87ff9da2cb513b4e35de034edeaa50d35
pekkansk <pekkansk@gmail.com>
2021-05-25 14:54:48 +0300

    Taulukosta kuvein haku ei toimi

--- javascript/koodi.js
+++ javascript/koodi.js
@@ -2,14 +2,30 @@
 window.addEventListener("load", ladattuna, );
 
 document.addEventListener("DOMContentLoaded", kuvat);
-	
+
+//Kuvataulukko
+var taulukonkuvat = ["https://i.ytimg.com/vi/hU_dcC3pvcU/maxresdefault.jpg",
+"https://i.ytimg.com/vi/JC6KKLz8qHE/hqdefault.jpg",
+"https://cdn.nettimoto.com/live/2020/07/25/11debd15c7693cbd-large.jpg",
+"https://cdn.nettimoto.com/live/2020/09/01/4a9f2e811f5a0568-large.jpg"
+];
+var len_kuvat = taulukonkuvat.length
+
+for(i = 0; i < len_kuvat; i++) {
+	var taulukonsisältö = "<img src=" + taulukonkuvat[i] + ">"
+}
+
+// Otsikon ja leipätekstin sisältö
 
 var Otsikkohiirella = "Sivu vaan kehittyy"
 var Otsikkoilmanhiirta = "Kes&aumlPekka"
 var Leipahiirella = "Muutoksen tuulet puhaltaa"
 var Leipailmanhiirta = "Hello world!"
+
 var Leipa = document.getElementById("leipa")
 var Otsikko = document.getElementById("otsikko")
+
+//Taulukon muuttaminen
 var taulukonleveys = 3
 var taulukonkorkeus = 2
 var taulukonkoko = taulukonleveys * taulukonkorkeus
@@ -17,6 +33,8 @@ var taulukonkoko = taulukonleveys * taulukonkorkeus
 
 function kuvat() {
 	console.log("Hello");
+	console.log(taulukonsisältö);
+	console.log(len_kuvat);
 	var IC = document.createElement("img");
   	IC.src = "https://i.ytimg.com/vi/hU_dcC3pvcU/maxresdefault.jpg";
 	var src = document.getElementById("ic");
@@ -31,10 +49,11 @@ function kuvat() {
 	for (i = 0; i < taulukonkorkeus; i++) { 
     	poyta.appendChild(tr.cloneNode(true));
 	}
-	cells = poyta.getElementsByTagName('td'); // get all of the cells
-	for (i = 0; i < taulukonkoko; i++) {               // number them
-    	cells[1].innerHTML = "<img src=\'https://i.ytimg.com/vi/JC6KKLz8qHE/hqdefault.jpg'>";
-		cells[2].innerHTML = "<img src=\'https://i.ytimg.com/vi/hU_dcC3pvcU/maxresdefault.jpg'>";
+	cells = poyta.getElementsByTagName('td'); 
+	for (i = 0; i < taulukonkoko; i++) {               // numeroidaan solut
+		console.log(taulukonsisältö);
+		cells[i].innerHTML = taulukonsisältö;
+
 	}
 	document.getElementById('container').appendChild(poyta);
 }
