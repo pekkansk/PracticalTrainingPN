d7b85d05e08a8649b6ad5e534d9159b905461ac8
pekkansk <pekkansk@gmail.com>
2021-05-25 10:55:11 +0300

    Valokuva

--- javascript/koodi.js
+++ javascript/koodi.js
@@ -1,4 +1,8 @@
-window.addEventListener("load", ladattuna)
+
+window.addEventListener("load", ladattuna, );
+
+document.addEventListener("DOMContentLoaded", kuvat);
+	
 
 var Otsikkohiirella = "Sivu vaan kehittyy"
 var Otsikkoilmanhiirta = "Kes&aumlPekka"
@@ -6,12 +10,24 @@ var Leipahiirella = "Muutoksen tuulet puhaltaa"
 var Leipailmanhiirta = "Hello world!"
 var Leipa = document.getElementById("leipa")
 var Otsikko = document.getElementById("otsikko")
+
+
+function kuvat() {
+	console.log("Hello");
+	var IC = document.createElement("img");
+  	IC.src = "https://i.ytimg.com/vi/hU_dcC3pvcU/maxresdefault.jpg";
+	var src = document.getElementById("ic");
+	src.appendChild(IC);
+	}
 	
 function ladattuna() {
+
 	Leipa.addEventListener("mouseover",hiiriYli);
 	Leipa.addEventListener("mouseout", hiiriOhi);
 	Otsikko.addEventListener("mouseover",hiiriYli);
 	Otsikko.addEventListener("mouseout", hiiriOhi);
+
+
 }
 function hiiriYli() {
 	Leipa.innerHTML = Otsikkohiirella
